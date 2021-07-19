79c644ecc1dfa1abec5f311d8b8f2347a337d2a2
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-07-19 12:33:19 -0600

    structured css

---
+++ script.js
@@ -0,0 +1,16 @@
+// API key for my OWM account
+var APIkey = 'c9299c81fa72cf0649fc417ca5d0c2b7'
+
+
+// click search button to save city lat/long using google places API
+
+// use search input to display today's date, current conditions (temp, humidity, wind speed, UV-index)
+
+var URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${APIkey}`;
+
+// 5 day forecast with the same data as before
+
+// store seached cities
+
+// clear searched cities
+
